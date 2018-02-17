85093025cf0dcf83ecd42db13e82673ed0607589
Jimmy Moon <ragingwind@gmail.com>
2018-02-17 20:24:40 +0900

    update test server

--- bin/next-workbox-start.js
+++ bin/next-workbox-start.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 const {join} = require('path')
 const {createServer} = require('http')
 const next = require('next')
@@ -5,14 +7,15 @@ const next = require('next')
 const app = next({dev: false})
 const handle = app.getRequestHandler()
 const port = Number.parseInt(process.argv.pop().split('=')[1] || 3000)
+const root = process.cwd()
 
 app.prepare().then(() => {
   createServer((req, res) => {
     if (req.url.startsWith('/sw')) {
-      app.serveStatic(req, res, join(__dirname, `./static/workbox/${req.url}`))
+      app.serveStatic(req, res, join(root, `./static/workbox/${req.url}`))
     }
     else if (req.url.startsWith('/workbox/')) {
-      app.serveStatic(req, res, join(__dirname, './static', req.url))
+      app.serveStatic(req, res, join(root, './static', req.url))
     } else {
       handle(req, res, req.url)
     }
